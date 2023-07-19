4d2c7c9013360b370a52dcf7fb16b63ba4123e5d
Tathagat017 <114250830+Tathagat017@users.noreply.github.com>
2023-07-19 21:22:49 +0530

    Inventory added

---
+++ buycars.com_frontend/frontendapp/src/Pages/InventoryPage.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Box,
+  useBreakpointValue,
+  Button,
+  Flex,
+  Input,
+} from "@chakra-ui/react";
+
+import TableRow from "./../Components/TableRow";
+import useDebounce from "../CutomHooks/UseThrottle";
+import { getData } from "../Redux/Inventory/actionCreater";
+
+const Inventory = () => {
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const isTableFullWidth = useBreakpointValue({ base: true, md: false });
+  const { token } = useSelector((store) => store.auth);
+  const { inventory_data } = useSelector((store) => store.inventory);
+  const dispatch = useDispatch();
+  //const [searchTerm, lastExecutedTime] = useDebounce(search, 200);
+  useEffect(() => {
+    dispatch(getData(token, page));
+    // console.log(oem_specs);
+  }, [page]);
+
+  const handlePageNext = (page) => {
+    setPage((page) => page + 1);
+  };
+
+  const handlePagePrev = (page) => {
+    setPage((page) => page - 1);
+  };
+
+  const handleSearch = () => {
+    dispatch(getData(token, page, search));
+  };
+
+  return (
+    <Box w={"100%"} overflowX="auto">
+      <TableContainer>
+        <Flex justifyContent={"space-evenly"}>
+          <Button onClick={handlePagePrev}>Previous</Button>
+          <Input
+            name="search"
+            placeholder="search model/Manufacturer/Year"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          ></Input>
+          <Button onClick={handleSearch}>Search</Button>
+          <Button onClick={handlePageNext}>Next</Button>
+        </Flex>
+        <Table size={isTableFullWidth ? "md" : "sm"}>
+          <TableCaption size="sm">OEM SPECIFICATIONS</TableCaption>
+
+          <Thead>
+            <Tr>
+              <Th>Manufacturer</Th>
+              <Th>Model</Th>
+              <Th>Year of Launch</Th>
+              <Th>Ex-showroom price</Th>
+              <Th>Colors</Th>
+              <Th>Mileage</Th>
+              <Th>Max Speed(km/h)</Th>
+              <Th>Power(BHP)</Th>
+              <Th>Image</Th>
+              <Th>Select</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {oem_specs?.map((el) => {
+              return <TableRow el={el} />;
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default Inventory;
